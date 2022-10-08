a7bb091753eb976636e133812c630e9387686cb6
Ifaz Ahmed <gdneop@gmail.com>
2022-10-08 12:52:33 +0100

    Create initial client application

---
+++ client/preload.js
@@ -0,0 +1,27 @@
+/**
+ * The preload script runs before. It has access to web APIs
+ * as well as Electron's renderer process modules and some
+ * polyfilled Node.js functions.
+ * 
+ * https://www.electronjs.org/docs/latest/tutorial/sandbox
+ */
+// expode node.js functionality
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld(
+  'electron',
+  {
+    doThing: () => ipcRenderer.send('do-a-thing')
+  }
+)
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
